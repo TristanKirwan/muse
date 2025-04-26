7f9ccc8d6e51bcb67d673bf08cfcaf26a47041db
Tristan <33831870+TristanKirwan@users.noreply.github.com>
2025-04-26 21:57:22 +0200

    Init repository

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+import "./globals.css";
+
+const interVariable = localFont({
+  display: "swap",
+  src: [
+    {
+      path: "../public/fonts/InterVariable-Italic.woff2",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/InterVariable.woff2",
+      style: "normal",
+    },
+  ],
+});
+
+export const metadata: Metadata = {
+  title: "Muse",
+  description: "Your inspiration in 1 spot",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${interVariable.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className='flex items-center justify-center min-h-screen'>
+      <h1 className='text-heading-1'>Hi Homie</h1>
+    </main>
+  );
+}
